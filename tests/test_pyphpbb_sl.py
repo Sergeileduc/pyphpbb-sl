046410e60e4f8c02c07a18abde22fb1639dbe1e8
Sergeileduc <sergei.leduc@gmail.com>
2020-07-30 13:21:29 +0200

    unit tests

--- tests/test_pyphpbb_sl.py
+++ tests/test_pyphpbb_sl.py
@@ -2,6 +2,7 @@
 
 """Tests for `pyphpbb_sl` package."""
 
+from collections import namedtuple
 import pytest
 
 
@@ -9,16 +10,45 @@ from pyphpbb_sl import PhpBB
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def cookies():
+    """Mock session cookies."""
+    Cookie = namedtuple('Cookie', ['key', 'value'])
+    return [Cookie("phpbb_pth98_u", 43533),
+            Cookie("phpbb_pth98_k", ""),
+            Cookie("phpbb_pth98_sid", "ffac899f2ff73")]
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+@pytest.mark.asyncio
+async def test_init_close():
+    phpbb = PhpBB("http://dummy.io")
+    await phpbb.close()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.mark.asyncio
+async def test_get_user_id(cookies):
+    """Test _get_user_id."""
+    def mock_cookies():
+        return cookies
+    async with PhpBB("http://dummy.io") as phpbb:
+        phpbb.browser.list_cookies = mock_cookies
+    assert phpbb._get_user_id() == 43533
+
+
+@pytest.mark.asyncio
+async def test_get_sid(cookies):
+    """Test get user SID."""
+    def mock_cookies():
+        return cookies
+    async with PhpBB("http://dummy.io") as phpbb:
+        phpbb.browser.list_cookies = mock_cookies
+    assert phpbb._get_sid() == "ffac899f2ff73"
+
+
+@pytest.mark.asyncio
+async def test_is_logged(cookies):
+    """Test get user SID."""
+    def mock_cookies():
+        return cookies
+    async with PhpBB("http://dummy.io") as phpbb:
+        phpbb.browser.list_cookies = mock_cookies
+        assert phpbb.is_logged()
