bd7b21b9ac81e1f10d4c143c0d5c5fdd2ae84f1b
Sergeileduc <sergei.leduc@gmail.com>
2020-07-31 21:56:34 +0200

    tools to explore sentbox as well, and delete messages

---
+++ examples/05-fetch-allready-read.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""Docstring."""
+
+# import aiohttp
+import asyncio
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from pyphpbb_sl import PhpBB
+
+logging.basicConfig(level=logging.INFO)
+
+# Parse a .env file and then load all the variables found as environment variables.  # noqa: E501
+load_dotenv()
+
+host = os.getenv("HOST")
+
+username = os.getenv("RECEIVER_NAME")
+password = os.getenv("RECEIVER_PASSWORD")
+sender = os.getenv("SENDER_NAME")
+
+
+logging.debug("host %s", host)
+logging.debug("username %s", username)
+logging.debug("password %s", password)
+
+
+# Context Manager code
+async def main():
+    async with PhpBB(host) as phpbb:
+        await phpbb.login(username, password)
+        unread_mess_list = await phpbb.fetch_read_messages()
+        print("Here are your read messages :")
+        filterd_mess_by_sender = [m for m in unread_mess_list if m['from_'] == sender]  # noqa: E501
+        print(*filterd_mess_by_sender, sep='\n')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
---
+++ examples/07-clean-sent-box.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""Docstring."""
+
+# import aiohttp
+import asyncio
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from pyphpbb_sl import PhpBB
+
+logging.basicConfig(level=logging.INFO)
+
+# Parse a .env file and then load all the variables found as environment variables.  # noqa: E501
+load_dotenv()
+
+host = os.getenv("HOST")
+
+username = os.getenv("SENDER_NAME")
+password = os.getenv("SENDER_PASSWORD")
+receiver = os.getenv("RECEIVER_NAME")
+
+logging.debug("host %s", host)
+logging.debug("username %s", username)
+logging.debug("password %s", password)
+logging.debug("receiver %s", password)
+
+
+# Context Manager code
+async def main():
+    async with PhpBB(host) as phpbb:
+        await phpbb.login(username, password)
+        sent_message_list = await phpbb.fetch_sent_messages()
+        filtered_sent_message_list = [m for m in sent_message_list if m['from_'] == receiver]  # noqa: E501
+        print(*filtered_sent_message_list, sep='\n')
+        for m in filtered_sent_message_list:
+            await phpbb.delete_mp(m)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
