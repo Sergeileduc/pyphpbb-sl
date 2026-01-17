0e3c34a5eebfe56b2dbc5d8cd047bf5dfde4a69a
Sergeileduc <sergei.leduc@gmail.com>
2026-01-17 02:14:04 +0100

    chore(settings/config):

--- tasks.py
+++ tasks.py
@@ -30,23 +30,23 @@ from invoke import task
 def get_platform():
     """Check the platform (Windos, Linux, or WSL)."""
     u = uname()
-    if u.system == 'Windows':
-        return 'windows'
-    elif u.system == 'Linux' and 'microsoft' in u.release:
-        return 'wsl'
+    if u.system == "Windows":
+        return "windows"
+    elif u.system == "Linux" and "microsoft" in u.release:
+        return "wsl"
     else:
-        return 'linux'
+        return "linux"
 
 
 def get_index_path():
     """Get full path for ./htmlcov/index.html file."""
     platform = get_platform()
     if platform != "wsl":
-        return Path('.').resolve() / 'htmlcov' / 'index.html'
+        return Path(".").resolve() / "htmlcov" / "index.html"
     # TODO: this part with .strip().replace() is ugly...
-    process = subprocess.run(['wslpath', '-w', '.'], capture_output=True, text=True)
-    pathstr = process.stdout.strip().replace('\\', '/')
-    return Path(pathstr) / 'htmlcov/index.html'
+    process = subprocess.run(["wslpath", "-w", "."], capture_output=True, text=True)
+    pathstr = process.stdout.strip().replace("\\", "/")
+    return Path(pathstr) / "htmlcov/index.html"
 
 
 # TASKS------------------------------------------------------------------------
@@ -62,36 +62,36 @@ def lint(c):
 def cleantest(c):
     """Clean artifacts like *.pyc, __pycache__, .pytest_cache, etc..."""
     # Find .pyc or .pyo files and delete them
-    exclude = ('venv', '.venv')
-    p = Path('.')
-    genpyc = (i for i in p.glob('**/*.pyc') if not str(i.parent).startswith(exclude))
-    genpyo = (i for i in p.glob('**/*.pyo') if not str(i.parent).startswith(exclude))
+    exclude = ("venv", ".venv")
+    p = Path(".")
+    genpyc = (i for i in p.glob("**/*.pyc") if not str(i.parent).startswith(exclude))
+    genpyo = (i for i in p.glob("**/*.pyo") if not str(i.parent).startswith(exclude))
     artifacts = chain(genpyc, genpyo)
     for art in artifacts:
         os.remove(art)
 
     # Delete caches folders
-    cache1 = p.glob('**/__pycache__')
-    cache2 = p.glob('**/.pytest_cache')
-    cache3 = p.glob('**/.mypy_cache')
+    cache1 = p.glob("**/__pycache__")
+    cache2 = p.glob("**/.pytest_cache")
+    cache3 = p.glob("**/.mypy_cache")
     caches = chain(cache1, cache2, cache3)
     for cache in caches:
         shutil.rmtree(cache)
 
     # Delete coverage artifacts
     with contextlib.suppress(FileNotFoundError):
-        os.remove('.coverage')
-        shutil.rmtree('htmlcov')
+        os.remove(".coverage")
+        shutil.rmtree("htmlcov")
 
 
 @task
 def cleanbuild(c):
     """Clean dist/, build/ and egg-info/."""
-    exclude = ('venv', '.venv')
-    p = Path('.')
-    gen1 = (i for i in p.glob('**/dist') if not str(i.parent).startswith(exclude))
-    gen2 = (i for i in p.glob('**/build') if not str(i.parent).startswith(exclude))
-    gen3 = (i for i in p.glob('**/*.egg-info') if not str(i.parent).startswith(exclude))
+    exclude = ("venv", ".venv")
+    p = Path(".")
+    gen1 = (i for i in p.glob("**/dist") if not str(i.parent).startswith(exclude))
+    gen2 = (i for i in p.glob("**/build") if not str(i.parent).startswith(exclude))
+    gen3 = (i for i in p.glob("**/*.egg-info") if not str(i.parent).startswith(exclude))
     builds = chain(gen1, gen2, gen3)
     for b in builds:
         shutil.rmtree(b)
@@ -100,7 +100,7 @@ def cleanbuild(c):
 @task
 def cleandoc(c):
     """Clean documentation files."""
-    p = Path('.').resolve() / "docs" / "build"
+    p = Path(".").resolve() / "docs" / "build"
     print(p)
     with contextlib.suppress(FileNotFoundError):
         shutil.rmtree(p)
@@ -120,25 +120,25 @@ def test(c):
 
 @task
 def coverage(c):
-    """Run unit-tests using pytest, with coverage reporting."""
-    # use the browser defined in varenv $BROWSER
-    # in WSL, if not set, example :  export BROWSER='/mnt/c/Program Files/Google/Chrome/Application/chrome.exe'  # noqa: E501
-    path = get_index_path()
-    # c.run('coverage run --source=tests -m pytest')
-    # c.run('coverage report -m')
-    # c.run('coverage html')
-    c.run('pytest --cov . --cov-report html')
-    webbrowser.open(path.as_uri())
+    """
+    Run test coverage and generate HTML report.
+    """
+    c.run("coverage erase", echo=True)
+    c.run("coverage run -m pytest", echo=True)
+    c.run("coverage html", echo=True)
+    print("\nHTML report generated in: htmlcov/index.html")
+    index = get_index_path()
+    webbrowser.open(index.as_uri())
 
 
 @task(cleandoc)
 def doc(c):
     c.run("pushd docs && make html && popd")
-    path = Path('.').resolve() / "docs" / "build" / "html" / "index.html"
+    path = Path(".").resolve() / "docs" / "build" / "html" / "index.html"
     webbrowser.open(path.as_uri())
 
 
 @task(cleanbuild)
 def build(c):
     """Build package using python -m build."""
-    c.run('python -m build')
+    c.run("python -m build")
